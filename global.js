b14d57f92c9f1411ced1ee6db404e5222eef276c
Rafael Tadeu <rafaeltadeusilva@gmail.com>
2023-05-10 15:08:44 -0300

    valida usuario

--- global.js
+++ global.js
@@ -1,2 +1,10 @@
-const url = "https://fenix-nu.vercel.app/";
-// const url = "http://localhost:5000/";
+// const url = "https://fenix-nu.vercel.app/";
+const url = "http://localhost:5000/";
+
+// function to validate if the user is logged in
+const isLoggedIn = () => {
+    if (localStorage.getItem("token")) {
+        return true;
+    }
+    return false;
+}
