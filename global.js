8112dc46bdcf7a93183fab5ff3634f2888b591ea
Rafael Tadeu <rafaeltadeusilva@gmail.com>
2023-05-10 15:26:55 -0300

    criação Debug Mode

--- global.js
+++ global.js
@@ -1,9 +1,11 @@
-// const url = "https://fenix-nu.vercel.app/";
-const url = "http://localhost:5000/";
+const isDebugMode = true;
+
+// const url = ;
+const url = isDebugMode ? "http://localhost:5000/" : "https://fenix-nu.vercel.app/";
 
 // function to validate if the user is logged in
 const isLoggedIn = () => {
-    if (localStorage.getItem("token")) {
+    if (isDebugMode || localStorage.getItem("token")) {
         return true;
     }
     return false;
