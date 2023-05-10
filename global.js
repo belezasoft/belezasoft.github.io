17a6a2f55cd5f2706151da6ee3c68c7fa55e5f1e
Rafael Tadeu <rafaeltadeusilva@gmail.com>
2023-05-10 16:31:30 -0300

    conversão de datas

--- global.js
+++ global.js
@@ -1,12 +1,23 @@
 const isDebugMode = false;
 
-// const url = ;
 const url = isDebugMode ? "http://localhost:5000/" : "https://fenix-nu.vercel.app/";
 
-// function to validate if the user is logged in
+// função para verificar se o usuário está logado
 const isLoggedIn = () => {
     if (isDebugMode || localStorage.getItem("token")) {
         return true;
     }
     return false;
+}
+
+// função para converter data br para data us
+const convertDateUS = (date) => {
+    const [day, month, year] = date.split("/");
+    return `${year}-${month}-${day}`;
+}
+
+// função para converter data us para data br
+const convertDateBR = (date) => {
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
 }
